b2736ac5cea913b5e7aaa10cc7d1a6741beddee7
itay-bardugo <itaybardugo91@gmail.com>
2021-01-13 23:49:15 +0200

    setup jwt ecosystem: jwks, jwe, jws. mid work.

--- knocqueue/services/authentication_ms/authentication/routes.py
+++ knocqueue/services/authentication_ms/authentication/routes.py
@@ -1,13 +1,22 @@
 from . import app
 from jwcrypto import jwk, jwe, jws, jwt
-from jwcrypto.common import json_encode
+from jwcrypto.common import json_encode, json_decode
 from authentication import settings
 from flask import request
 
 
 @app.route('/')
 def index():
-    return 'hi!'
+    public_file = (open(str(settings.ROOT) + "/public.pub", "r"))
+    public_key = jwk.JWK.from_pem(public_file.read().encode())
+    ks = jwk.JWKSet()
+    ks.add(public_key)
+    return ks.export(as_dict=True)
+
+    ks2 = jwk.JWKSet()
+    ks2.import_keyset(ks.export(False))
+    kkk = ks2.get_key(public_key.key_id)
+    return kkk.export_to_pem(True, None)
 
 
 @app.route("/keyPairs")
@@ -35,13 +44,12 @@ def encrypt():
     import time
     t = round(time.time()) + 10
     payload = {"exp": t, "data": {'itay': 'itay'}}
-
     header = {"alg": "RS256", "kid": private_key.thumbprint()}
     jwttoken = jwt.JWT(header=header, claims=payload)
     jwttoken.make_signed_token(private_key)
     payload = jwttoken.serialize(True)
 
-    encrypt_header = {"alg": "RSA-OAEP", "enc": "A128CBC-HS256"}
+    encrypt_header = {"kid": private_key.thumbprint(), "alg": "RSA-OAEP", "enc": "A128CBC-HS256"}
     jwttoken_e = jwt.JWT(header=encrypt_header, claims=payload)
     jwttoken_e.make_encrypted_token(public_key)
     payload = jwttoken_e.serialize(True)
@@ -59,7 +67,13 @@ def decrypt():
 
     e = jwt.JWT(key=private_key, jwt=enc)
     s = jwt.JWT()
-    s.leeway = 0
+    s.leeway = 999999
     s.deserialize(jwt=e.claims, key=public_key)
-    import json
-    return json.loads(s.claims)
+
+    # it can be validated from keyset
+    #ks = jwk.JWKSet()
+    #ks.import_keyset(json_encode(index()))
+    #s.deserialize(jwt=e.claims, key=ks.get_key(json_decode(e.header)["kid"]))
+
+
+    return json_decode(s.claims)
