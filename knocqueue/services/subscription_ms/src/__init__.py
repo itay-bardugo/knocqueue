370ed23151006d18dd87401d9f95572d452eea78
itay-bardugo <itaybardugo91@gmail.com>
2021-01-19 00:08:50 +0200

    setup subscription model (midwork)

--- knocqueue/services/subscription_ms/src/__init__.py
+++ knocqueue/services/subscription_ms/src/__init__.py
@@ -2,9 +2,23 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_restful import Api
 from src import settings
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_CONNECTION_STRING", "")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, directory=settings.MIGRATION_PATH)
+api = Api(app)
+
+
+class ServiceToServiceHandshakeMiddleWare:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        import os
+        import requests
+        requests.post(
+            os.environ.get("AUTHENTICATION_SERVICE_TO_SERVICE_VERIFY_ENDPOINT"),
+        )
--- knocqueue/services/subscription_ms/src/models/__init__.py
+++ knocqueue/services/subscription_ms/src/models/__init__.py
@@ -1,8 +1,10 @@
 from src import db
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class Subscription(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
     name = db.Column(db.String(255))
     email = db.Column(db.String(255))
     password = db.Column(db.String(255))
