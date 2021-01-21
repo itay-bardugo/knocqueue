e11c34ae204c76986625926c7bfaee7116a5c997
itay-bardugo <itaybardugo91@gmail.com>
2021-01-22 00:11:18 +0200

    one entrypoint for all registration methods - mid work

---
+++ knocqueue/services/subscription_ms/src/controllers/register.py
@@ -0,0 +1,16 @@
+from flask_restful import Resource
+from src.boot.registration_factories import factory
+
+
+class Register(Resource):
+    # key is the method, value is the factory alias
+    __map = {
+        'simple': 'credentials'
+    }
+
+    def post(self, method):
+        try:
+            register = factory.make(Register.__map[method])
+            return register.register()
+        except KeyError:
+            return 'Bad Request', 400
