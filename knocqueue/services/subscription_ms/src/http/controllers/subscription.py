a445a714065c7cfb97a638b96fe287db5cfeeceb
itay-bardugo <itaybardugo91@gmail.com>
2021-01-26 19:18:34 +0200

    allow unknown fields

--- knocqueue/services/subscription_ms/src/http/controllers/subscription.py
+++ knocqueue/services/subscription_ms/src/http/controllers/subscription.py
@@ -11,7 +11,7 @@ class Subscription(Resource):
         'simple': 'credentials'
     }
 
-    def post(self, method):
+    def get(self, method):
         try:
             register = factory.make(Subscription.__map[method])
             return register.register(request.args)
@@ -20,5 +20,5 @@ class Subscription(Resource):
         except KeyError:
             return 'Bad Request', 400
 
-    def get(self):
-        return 'get'
+    # def get(self):
+    #     return 'get'
--- knocqueue/services/subscription_ms/src/http/schema/subscription/__init__.py
+++ knocqueue/services/subscription_ms/src/http/schema/subscription/__init__.py
@@ -1,7 +1,9 @@
-from marshmallow import Schema, fields, ValidationError
+from marshmallow import Schema, fields, ValidationError, INCLUDE
 
 
 class _Registration(Schema):
+    class Meta:
+        unknown = INCLUDE
     email = fields.Email(required=True, data_key='email')
     password = fields.Str(required=True, data_key='password')
     allow_news_letter = fields.Int(required=False, data_key='allowNewsLetter')
