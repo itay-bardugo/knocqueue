8a58fbb70929b3b28d6d8bef3988021912e1c961
itay-bardugo <itaybardugo91@gmail.com>
2021-01-29 01:34:38 +0200

    build new statements builder

---
+++ knocqueue/links/knocqueue_utils/statement.py
@@ -0,0 +1,83 @@
+from typing import Callable, Any
+import abc
+
+
+class _BaseThen(metaclass=abc.ABCMeta):
+    def __init__(self, checks: set):
+        self._confirmed = any(checks)
+        self._flag = False
+
+    def _do_if_confirmed(self, callable_: Callable[..., Any]):
+        if self._flag:
+            raise Exception('--preventing from executing twice--')
+
+        if self._confirmed:
+            self._flag = True
+            return callable_()
+
+    def raise_an_error(self, exception: BaseException):
+        def _():
+            raise exception
+
+        self._do_if_confirmed(_)
+        return self
+
+
+class _Otherwise(_BaseThen):
+    ...
+
+
+class _Then(_BaseThen):
+    @property
+    def otherwise(self):
+        return _Otherwise({True if self._flag is False else False})
+
+
+class _Expression:
+    def __init__(self):
+        self._checks = set()
+        self._value = None
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, v):
+        self._value = v
+
+    def _setup(self):
+        vars_ = vars(self)
+        vars_ = {k: None for k in vars_}
+        return self
+
+    def _happens(self):
+        return bool(self.value)
+
+    def happens(self):
+        self._add_check(self._happens())
+        return self
+
+    def _add_check(self, expression):
+        self._checks.add(expression)
+
+    def not_happens(self):
+        self._add_check(not self._happens())
+
+    @property
+    def then(self):
+        return _Then(self._checks)
+
+
+class When(type):
+    _expression = _Expression()
+
+    def __new__(mcs, expression: bool):
+        if mcs._expression is None:
+            mcs._expression = mcs._Expression()
+        mcs._expression._setup()
+        mcs._expression.value = expression
+        return mcs._expression
+
+    def kill(cls):
+        cls._expression = None
