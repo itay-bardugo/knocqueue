2bcb95055e9461fcee4ee9c07bb36eb11ee66b88
itay-bardugo <itaybardugo91@gmail.com>
2021-02-10 23:56:42 +0200

    handle errors

--- knocqueue/services/subscription_ms/src/http/schema/subscription/__init__.py
+++ knocqueue/services/subscription_ms/src/http/schema/subscription/__init__.py
@@ -1,12 +1,13 @@
 from src.repositories.subscription import SubscriptionRepository
 from typing import Type
 from knocqueue_utils import schema
-from marshmallow import ValidationError
+from marshmallow import ValidationError, validate
 
 
 class Registration(schema.BaseSchema):
     email = schema.Email(required=True, data_key='email')
-    password = schema.Str(required=True, data_key='password')
+    password = schema.Str(required=True, validate=[validate.Length(min=6, max=16, error='ms-password-length')],
+                          data_key='password')
     allow_news_letter = schema.Bool(required=False, data_key='allowNewsLetter')
     _repository: Type[SubscriptionRepository]
 
@@ -14,7 +15,7 @@ class Registration(schema.BaseSchema):
         self.when(self._repository.get_by_email(data['email'])) \
             .happens() \
             .then \
-            .raise_an_error(ValidationError({'code': 405}))
+            .raise_an_error(ValidationError('ms-user-exists'))
 
 
 class CredentialsRegistrationSchema(Registration):
