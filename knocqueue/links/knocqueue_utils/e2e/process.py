b99e67776cea96a39acf1ab10d685e85148b0066
itay-bardugo <itaybardugo91@gmail.com>
2021-02-15 22:36:37 +0200

    move to Generics

--- knocqueue/links/knocqueue_utils/e2e/process.py
+++ knocqueue/links/knocqueue_utils/e2e/process.py
@@ -1,16 +1,24 @@
 import abc
-from typing import Type
+from typing import Type, Generic, TypeVar
 from knocqueue_utils.schema import BaseSchema
 from knocqueue_utils.repository import Repository
 
+_Repository = TypeVar('_Repository', bound=Repository)
+_Schema = TypeVar('_Schema', bound=BaseSchema)
+_Model = TypeVar('_Model')
 
-class Process(metaclass=abc.ABCMeta):
-    def __init__(self, *, repository: Type[Repository], schema: Type[BaseSchema]):
+
+class Process(Generic[_Model], metaclass=abc.ABCMeta):
+    def __init__(self, *, repository: Type[_Repository], schema: Type[_Schema]):
         self._repository = repository
         self._schema = schema(repository=self._repository)
+        self.__model: _Model = None
 
     def _validate(self, payload):
-        self._schema.load(payload)  # validate the payload
+        self.__model = self._schema.load(payload)  # validate the payload
+
+    def model(self) -> _Model:
+        return self.__model
 
     def execute(self, payload: dict):
         self._validate(payload)
