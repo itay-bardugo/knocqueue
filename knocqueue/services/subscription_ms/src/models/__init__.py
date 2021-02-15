765221b1a204bbabe2e316cab5b9319f6e8d0fe5
itay-bardugo <itaybardugo91@gmail.com>
2021-02-15 22:35:33 +0200

    typing RegisterService

--- knocqueue/services/subscription_ms/src/models/__init__.py
+++ knocqueue/services/subscription_ms/src/models/__init__.py
@@ -9,4 +9,5 @@ class Subscription(db.Model):
     email = db.Column(db.String(255))
     password = db.Column(db.String(255))
     created_at = db.Column(db.TIMESTAMP, server_default=db.func.now())
+    allow_news_letter = db.Column(db.Boolean, default=False)
     last_update = db.Column(db.TIMESTAMP, server_default=db.func.now(), onupdate=db.func.current_timestamp())
