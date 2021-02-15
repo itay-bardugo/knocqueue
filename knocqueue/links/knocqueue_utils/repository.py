5799db48253d49f8f8b6ee1675b7c3855807d434
itay-bardugo <itaybardugo91@gmail.com>
2021-02-15 09:26:20 +0200

    handle filter_by actions from repository

--- knocqueue/links/knocqueue_utils/repository.py
+++ knocqueue/links/knocqueue_utils/repository.py
@@ -1,12 +1,21 @@
 import abc
 from flask_sqlalchemy.model import Model
-from typing import TypeVar, Generic, Union
+from typing import TypeVar, Generic, Union, Type, List
+
 T = TypeVar('T', bound=Model)
 
 
 class Repository(Generic[T], metaclass=abc.ABCMeta):
-    _model: T = None
+    _model: Type[T] = None
 
     @classmethod
     def get_by_pk(cls, pk) -> Union[T, None]:
         return cls._model.query.get(pk)
+
+    @classmethod
+    def filter_by_first(cls, **kwargs) -> Union[T, None]:
+        return cls._model.query.filter_by(**kwargs).first()
+
+    @classmethod
+    def filter_by_all(cls, **kwargs) -> List[T]:
+        return cls._model.query.filter_by(**kwargs).all()
