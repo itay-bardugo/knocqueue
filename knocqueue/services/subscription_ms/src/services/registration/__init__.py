ba41dc9f5a4f0997b28487fdfb2bdb2f97f32ee9
itay-bardugo <itaybardugo91@gmail.com>
2021-02-15 22:36:06 +0200

    typing RegisterService

--- knocqueue/services/subscription_ms/src/services/registration/__init__.py
+++ knocqueue/services/subscription_ms/src/services/registration/__init__.py
@@ -1,8 +1,9 @@
 from knocqueue_utils.e2e.process import Process
 from src.strategies.registration import RegistrationStrategy
+from src.models import Subscription
 
 
-class RegisterService(Process):
+class RegisterService(Process[Subscription]):
 
     def __init__(self, **kwargs):
         self._strategy: RegistrationStrategy = kwargs.pop('strategy')
@@ -10,20 +11,3 @@ class RegisterService(Process):
 
     def _execute(self):
         return self._strategy.register()
-
-#
-# class RegisterService2:
-#     def __init__(self, repository: Type[Repository], strategy: RegistrationStrategy, schema: Type[Registration]):
-#         self._repository = repository
-#         self._strategy = strategy
-#         self._schema = schema(repository=self._repository)
-#
-#     def _validate(self, payload):
-#         self._schema.load(payload)  # validate the payload
-#
-#     def register(self, payload: dict):
-#         self._validate(payload)
-#         return self._register()
-#
-#     def _register(self):
-#         return self._strategy.register()
