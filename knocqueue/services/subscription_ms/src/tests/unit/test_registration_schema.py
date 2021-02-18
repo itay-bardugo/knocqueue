a2777e129c37876907eabda91ada21ab43e0c74a
itay-bardugo <itaybardugo91@gmail.com>
2021-02-19 00:01:03 +0200

    testing: fix fixture

--- knocqueue/services/subscription_ms/src/tests/unit/test_registration_schema.py
+++ knocqueue/services/subscription_ms/src/tests/unit/test_registration_schema.py
@@ -5,17 +5,17 @@ from src.http.schema.subscription import CredentialsRegistrationSchema, Subscrip
 from marshmallow import ValidationError
 
 
-class TestCredentialsSchema(TestCase):
+class TestRegistrationSchema(TestCase):
     def setUp(self) -> None:
-        self.schema = CredentialsRegistrationSchema(repository=SubscriptionRepository)
+        self.registration_schema = CredentialsRegistrationSchema(repository=SubscriptionRepository)
 
     @patch('src.http.schema.subscription.SubscriptionRepository.filter_by_first')
     def test_it_doesnt_fail_when_email_not_exist(self, filter_by_first):
         filter_by_first.return_value = None
-        self.schema._email_not_exists()
+        self.registration_schema._email_not_exists('test')
 
     @patch('src.http.schema.subscription.SubscriptionRepository.filter_by_first')
     def test_it_does_fail_when_email_not_exist(self, filter_by_first):
         filter_by_first.return_value = True
         with self.assertRaises(ValidationError):
-            self.schema._email_not_exists('test')
+            self.registration_schema._email_not_exists('test')
