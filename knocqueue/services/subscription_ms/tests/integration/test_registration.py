7f029a1d61e91e5731ae4bb8c4921d289f8d49c5
itay-bardugo <itaybardugo91@gmail.com>
2021-02-22 08:42:04 +0200

    minor changes

--- knocqueue/services/subscription_ms/tests/integration/test_registration.py
+++ knocqueue/services/subscription_ms/tests/integration/test_registration.py
@@ -4,12 +4,12 @@ from unittest.mock import patch
 
 class TestRegistration(BaseTestCase):
     @patch('src.factories.registration.RegisterService.execute')
-    def test_it_responds_with_200_on_simple_registration(self, registration_endpoint):
+    def test_it_responds_200_on_simple_registration(self, registration_endpoint):
         registration_endpoint.return_value = self.response('OK')
         response = self.app.post('/subscription/register/simple')
         self.assertEqual(200, response.status_code)
 
     @patch.dict('src.routes.Subscription._Subscription__map', {'pass': 'pass'}, clear=True)
-    def test_it_responds_with_404_on_undefined_registration_type(self):
+    def test_it_responds_404_on_undefined_registration_type(self):
         response = self.app.post('/subscription/register/invalid')
         self.assertEqual(404, response.status_code)
